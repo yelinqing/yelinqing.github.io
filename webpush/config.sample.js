6683523c12f33523ce288c94ef33291039248b9a
小叶 <linqye@126.com>
2017-06-10 15:57:17 +0800

    Create config.sample.js
    add apiKey

--- webpush/config.sample.js
+++ webpush/config.sample.js
@@ -1,4 +1,4 @@
 window.GoogleSamples = window.GoogleSamples || {};
 window.GoogleSamples.Config = window.GoogleSamples.Config || {
-  gcmAPIKey: '<Your Cloud Messaging API Key from https://console.firebase.google.com>'
+  gcmAPIKey: 'AIzaSyDPKHuudtjCESoqNoA4wt1gditSCgZ9jGA'
 };
