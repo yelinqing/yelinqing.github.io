e16e8311934c68cbc831e18c646b502a2d80e488
yelinqing <linqye@126.com>
2018-01-05 16:31:02 +0800

    modify main.js

--- pwatest/main.js
+++ pwatest/main.js
@@ -236,9 +236,9 @@ window.addEventListener('load', function() {
   // Check that service workers are supported, if so, progressively
   // enhance and add push messaging support, otherwise continue without it.
   if ('serviceWorker' in navigator) {
-    // navigator.serviceWorker.register('./service-worker.js')
-    // .then(initialiseState);
-      navigator.serviceWorker.register('./service-worker.js');
+    navigator.serviceWorker.register('./service-worker.js')
+    .then(initialiseState);
+      // navigator.serviceWorker.register('./service-worker.js');
   } else {
     window.Demo.debug.log('Service workers aren\'t supported in this browser.');
   }
