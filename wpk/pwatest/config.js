e068ad296e254aa9c89b7a1c3bff08522db8a103
叶林清 <linqing.ylq@alibaba-inc.com>
2019-06-06 14:21:08 +0800

    add servicework error case

---
+++ wpk/pwatest/config.js
@@ -0,0 +1,8 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AIzaSyDPKHuudtjCESoqNoA4wt1gditSCgZ9jGA',
+  APPLICATION_KEYS: {
+  	publicKey: 'BNLAD3ezY6MmFLVQlVgCIHyuYKSSOM22R_KlC8K5rvdkAQUlQN47Yc9FbZOoIG4bB0WcqEWYxwyrg6V2VHfwHNQ',
+  	privateKey: 'Iy3ovYsa4ATTY6_FCnFFJgnwDhkpv8mztK5IIn5hpzo'
+  }
+};
